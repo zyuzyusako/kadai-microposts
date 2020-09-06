de034fcc7ef06767f2581215cb045d6db6fd7784
zyuzyu.sako <sakosakozaq12wsx@gmail.com>
2020-09-06 02:27:46 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,10 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
+
+// ユーザ登録
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
