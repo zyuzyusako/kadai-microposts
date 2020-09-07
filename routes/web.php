8226a0b88f4f565c0543a97fb519a770629e3a8c
zyuzyu.sako <sakosakozaq12wsx@gmail.com>
2020-09-07 02:28:45 +0000

    user pages

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,12 @@ Route::get('/', function () {
 // ユーザ登録
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
+// 認証
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+});
