66755508e2a57b103546b11cee6e0b93ee2b111b
zyuzyu.sako <sakosakozaq12wsx@gmail.com>
2020-09-11 09:14:51 +0000

    post

--- routes/web.php
+++ routes/web.php
@@ -21,15 +21,14 @@ Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
-Route::group(['middleware' => ['auth']], function () {
+
     Route::group(['prefix' => 'users/{id}'], function () {
         Route::post('follow', 'UserFollowController@store')->name('user.follow');
         Route::delete('unfollow', 'UserFollowController@destroy')->name('user.unfollow');
         Route::get('followings', 'UsersController@followings')->name('users.followings');
         Route::get('followers', 'UsersController@followers')->name('users.followers');
     });
-
-    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
-
-    Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
+ Route::group(['middleware' => ['auth']], function () {
+        Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+        //Route::resource('Tasklists', 'MicropostsController', ['only' => ['store', 'destroy']]);
 });
